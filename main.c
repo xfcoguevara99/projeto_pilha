7cc64c9b540818175d302a0e354416d9aee62693
francisco <francisco.g1999@aluno.ifsc.edu.br>
2025-04-29 17:18:18 -0300

    continuando com a estrutura da fila

--- main.c
+++ main.c
@@ -3,24 +3,15 @@
 #include <string.h>
 #include "calculadora.h"
 int main(void) {
-   /*pilha_t pilha_t *p = criar_pilha();
-    pop(p);
-    push(p,3);
-    push(p,5);
-    push(p,7);
-    push(p,5);
-    push(p,7);
-    push(p,7);
-    push(p,5);
-    push(p,7);
-    return 0;*/
-    char expressao[10];
-    int indice;
-    printf("escreva a expressao em formato polonesa: ");
-    fgets(expressao,sizeof(expressao),stdin);
-    indice = strcspn(expressao,"\n");
-    expressao[indice] = 0;
-    int resultado = notacao_polonesa_reversa(expressao);
-    printf("resultado: %f",resultado);
+   fila_t *fila=create_queue();
+
+   printf("%d",empty_queue(fila));
+
+
+
+
+
+
+
 
 }
