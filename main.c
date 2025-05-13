e3dc9383756b7c1b6180bd7004cc768fe44a07b9
francisco <francisco.1999@aluno.ifsc.edu.br>
2025-05-13 17:27:38 -0300

    testeste lista

--- main.c
+++ main.c
@@ -4,21 +4,13 @@
 #include "calculadora.h"
 int main(void) {
    lista_t  *lista;
-   lista = create_list();
-   add_element_to_list(lista,5);
-   add_element_to_list(lista,3);
+   lista = create_list(true);
    add_element_to_list(lista,1);
-   add_element_to_list(lista,0);
-   search(lista,1);
-   add_element_to_list(lista,7);
-   add_element_to_list(lista,6);
-   add_element_to_list(lista,8);
-   add_element_to_list(lista,2);
-   add_element_to_list(lista,8);
-   add_element_to_list(lista,2);
+   add_element_to_list(lista,3);
    add_element_to_list(lista,5);
-   remover(lista,1);
-   add_element_to_list(lista,15);
+   add_element_to_list(lista,6);
+   add_element_to_list(lista,2);
+
 
 
 
