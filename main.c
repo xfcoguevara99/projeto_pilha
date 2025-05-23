b94676889e50e55183b7d444cfbede41187ad99c
francisco <francisco.g1999@aluno.ifsc.edu.br>
2025-05-23 17:28:27 -0300

    teste da lista encadeada

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 #include <string.h>
 #include "calculadora.h"
 int main(void) {
-   no_list_enc_t *lista;
-   lista = create_list(1);
-   add_element_to_list_enc(2);
+   no_t *lista;
+   lista = create_no_list_enc(1);
+   lista = add_element_to_list_enc(lista,2);
+   lista = add_element_to_list_enc(lista,3);
+   lista = add_element_to_list_enc(lista,4);
+   lista = add_element_to_list_enc(lista,5);
+   //testar funcao encontrar
+   //no_t *encontrado = search_element_to_list_enc(lista,6);
+   remove_element_to_list_enc(lista,1);
 
 
 
